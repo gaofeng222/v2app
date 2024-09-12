5ffce134f139a4bab5f97fa86f958aad8736923a
gaofeng222 <596642721@qq.com>
2024-09-12 18:04:22 +0800

    fix:prop的测试

---
+++ src/components/c-checkbox/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label>
+      {{ value }}
+      <input type="checkbox" :checked="checked" @change="handleChange" />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CCheckbox",
+  model: {
+    prop: "checked",
+    event: "change",
+  },
+  props: {
+    checked: Boolean,
+    value: {
+      type: String,
+      default: "复选框",
+    },
+  },
+  methods: {
+    handleChange($event) {
+      this.$emit("change", $event.target.checked);
+    },
+  },
+};
+</script>
+<style lang="less" scoped>
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,47 @@
 <template>
   <g-container>
-    <div>home首页</div>
+    <div>
+      <el-row :gutter="10">
+        <el-col :span="6">
+          <el-card>
+            <div>
+              {{ logVue }}
+              <CCheckbox v-model="logVue" value="自定义复选" />
+            </div>
+          </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card>
+            <div></div>
+          </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card>
+            <div></div>
+          </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card>
+            <div></div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
   </g-container>
 </template>
 
 <script>
-export default {}
+import CCheckbox from "@/components/c-checkbox";
+export default {
+  components: {
+    CCheckbox,
+  },
+  data() {
+    return {
+      logVue: true,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
